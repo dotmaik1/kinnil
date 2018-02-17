a866d5fb75f9a4836c1bec1ffc026555cf70ea72
dotmaik1 <dotmaik1@gmail.com>
2018-02-16 18:39:33 -0700

    Se agrego inicio y dispatch/to be asigned
    Las puse aquí provisional, hay que molerlas a donde sea mas conveniente

--- app/controller/web/user_controller.js
+++ app/controller/web/user_controller.js
@@ -6,4 +6,16 @@ router.get('/signup', (req, res) => {
     res.render('pages/signup.ejs', { message: ''});
 });
 
+router.get('/login', (req, res) => {
+    res.render('pages/login.ejs', { message: ''});
+});
+
+router.get('/inicio', (req, res) => {
+    res.render('pages/inicio.ejs', { message: ''});
+});
+
+router.get('/dispatch/to-be-asigned', (req, res) => {
+    res.render('pages/dispatch-to-be-asigned.ejs', { message: ''});
+});
+
 exports.router = router;
